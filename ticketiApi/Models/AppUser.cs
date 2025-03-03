86b4d1293b077eadf821278490f972284dd6580e
engr-Godswillochi <ochigodswill@gmail.com>
2025-03-03 23:05:22 +0100

    Initial Commit

---
+++ ticketiApi/Models/AppUser.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ticketiApi.Models
+{
+    public class User
+    {
+        
+    }
+}
