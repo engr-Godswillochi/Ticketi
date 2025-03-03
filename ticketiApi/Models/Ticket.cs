7cfa177ccd8ebd0217e9cc33ce9bad76ab851497
engr-Godswillochi <ochigodswill@gmail.com>
2025-03-03 23:34:18 +0100

    ticket model

--- ticketiApi/Models/Ticket.cs
+++ ticketiApi/Models/Ticket.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,18 @@ namespace ticketiApi.Models
 {
     public class Ticket
     {
+        public int Id { get; set; } // Can be int or string
+        [Required]
+        public required string Name { get; set; }
         
+        public decimal Price { get; set; }
+        public TicketType Type{ get; set; } = TicketType.REGULAR;
+        public int Available { get; set; }
+        public int Total { get; set; }
+    }
+    public enum TicketType {
+        VIP,
+        VVIP,
+        REGULAR
     }
 }
