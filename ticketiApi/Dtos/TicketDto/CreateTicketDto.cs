a6c0ac2d59e2d5b461cf84b342532fcff124bf5e
engr-Godswillochi <ochigodswill@gmail.com>
2025-03-03 23:57:05 +0100

    updating dtos

--- ticketiApi/Dtos/TicketDto/CreateTicketDto.cs
+++ ticketiApi/Dtos/TicketDto/CreateTicketDto.cs
@@ -2,16 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ticketiApi.Models;
 
 namespace ticketiApi.Dtos.TicketDto
 {
     public class CreateTicketDto
     {
-    public object Id { get; set; } // Can be int or string
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-    public string Type { get; set; } // "REGULAR", "VIP", "VVIP" or other
-    public int Available { get; set; }
-    public int Total { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public TicketType Type { get; set; } 
+        public int Available { get; set; }
+        public int Total { get; set; }
     }
 }
--- ticketiApi/Models/Event.cs
+++ ticketiApi/Models/Event.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ticketiApi.Models
 {
+    [Table("Events")]
     public class Event
     {
         public int Id { get; set; }
