662e72190c3ba72be75a0fcdd1e213cb0810f870
engr-Godswillochi <ochigodswill@gmail.com>
2025-03-21 23:16:42 +0100

    create

--- ticketiApi/Mappers/TicketMapper.cs
+++ ticketiApi/Mappers/TicketMapper.cs
@@ -2,21 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ticketiApi.Dtos.TicketDto;
 using ticketiApi.Models;
 
 namespace ticketiApi.Mappers
 {
     public static class TicketMapper
     {
-        public static Ticket ToUpdateTicketDto (this UpdateTicket updateTicket)
+        public static Ticket ToCreateTicketDto (this CreateTicketDto     createTicketDto)
         {
             return new Ticket
             {
-                Name = updateTicket.Name,
-                Price = updateTicket.Price,
-                Type = updateTicket.Type,
-                Available = updateTicket.Available,
-                Total = updateTicket.Total
+                Name = createTicketDto.Name,
+                Price = createTicketDto.Price,
+                Type = createTicketDto.Type,
+                Available = createTicketDto.Available,
+                Total = createTicketDto.Total
             };
         }
     }
